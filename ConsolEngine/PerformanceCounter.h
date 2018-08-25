bd80b5eaa77226fff11da71e915a4f685eec437b
Katya Granat <k.granat2017@.com>
2018-08-25 16:39:33 +0300

    initial commit

---
+++ ConsolEngine/PerformanceCounter.h
@@ -0,0 +1,45 @@
+// Copyright 2009-2014 Blam Games, Inc. All Rights Reserved.
+
+#include <windows.h>
+
+class CStopwatch
+{
+public:
+	CStopwatch();
+	void Start();
+	int Now();
+	int Time();
+private:
+	LARGE_INTEGER m_liPerfFreq;
+	LARGE_INTEGER m_liPerfStart;
+};
+
+#ifdef MY_PERFORMENCE_COUNTER
+
+CStopwatch::CStopwatch()
+{
+    QueryPerformanceFrequency(&m_liPerfFreq);//получает частоту производительности   с запуска проги MY_PERFORMENCE_COUNTER в секкунду
+    Start();
+}
+
+void CStopwatch::Start()
+{
+    QueryPerformanceCounter(&m_liPerfStart);//сколько раз вызывается с запуска проги
+}
+
+int CStopwatch::Now()
+{
+    // возвращает число миллисекунд после вызова Start
+        LARGE_INTEGER liPerfNow;
+        QueryPerformanceCounter(&liPerfNow);
+	return (((liPerfNow.QuadPart - m_liPerfStart.QuadPart) * 1000) / m_liPerfFreq.QuadPart);
+}
+
+int CStopwatch::Time()
+{
+    LARGE_INTEGER liPerfNow;
+	QueryPerformanceCounter(&liPerfNow);
+	return ((liPerfNow.QuadPart * 1000) / m_liPerfFreq.QuadPart);
+}
+
+#endif
